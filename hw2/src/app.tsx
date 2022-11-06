916dcaafa279e8bb00901c89035e2c0e1b3a5241
Sergey Gelagaev <sgelagaev@it-one.ru>
2022-11-06 04:53:57 +0300

    hw2 update storybook

--- hw2/src/app.tsx
+++ hw2/src/app.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
 import './app.css';
+import React, { useState } from 'react';
+import { Button } from './stories/components/button/Button';
 
 export function App() {
-  const [count, setCount] = useState(0);
-  return (
-    <div className="red">
-      {count}
-      <button onClick={() => setCount(count + 1)}>Click me!</button>
-    </div>
-  );
+    const [count, setCount] = useState(0);
+    return (
+        <div className="red">
+            {count}
+            <Button size="small" primary onClick={() => setCount(count + 1)} label="ADD 1"/>
+        </div>
+    );
 }
